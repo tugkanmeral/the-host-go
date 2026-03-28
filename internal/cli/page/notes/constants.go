62e808682fff94e28367a2b325969085c422898d
Tuğkan Meral <tugkan.mrl@gmail.com>
2026-03-28 09:42:00 +0300

    changegd DefaultListTake constant

--- internal/cli/page/notes/constants.go
+++ internal/cli/page/notes/constants.go
@@ -3,6 +3,6 @@ package notes
 // Lines reserved above/below the list scroll area (title + paging banner + spacing + nav hint).
 const ListViewFrameLines = 5
 
-const DefaultListTake = 5
+const DefaultListTake = 2
 
 const MaxListTake = 100
